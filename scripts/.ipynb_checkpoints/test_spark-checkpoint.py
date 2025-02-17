1c371baf41382bdd1df4c9e1a24a3f671e58765b
Bymombela <bymombela@gmail.com>
2025-02-16 22:05:28 -0600

    Version final

--- scripts/.ipynb_checkpoints/test_spark-checkpoint.py
+++ scripts/.ipynb_checkpoints/test_spark-checkpoint.py
@@ -1,9 +1,8 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
-    .master("spark://172.20.0.6:7077") \
+    .master("spark://spark-master:7077") \
     .appName("AmbientTest") \
     .getOrCreate()
 
-print(spark.range(100).count())
-
+print(spark.range(100).count())  # Debe imprimir 100
